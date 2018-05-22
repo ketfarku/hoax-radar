65bf002260b192ea6dd4fb0090bcaccde42783f7
lazos <lajos.bencz@gmail.com>
2018-05-22 14:15:05 +0200

    sync

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@
             padding: 0;
             height: 100%;
             min-height: 100%;
-            background: url('/img/mkkp-logo.jpg') no-repeat center;
+            background: url('/img/mkkp-logo.png') no-repeat center;
             background-size: contain;
         }
     </style>
