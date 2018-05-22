679ecd0d2304ddaf00ae53bb96f5d9d2acb0454c
lazos <lajos.bencz@gmail.com>
2018-05-22 12:29:39 +0200

    sync

--- public/index.php
+++ public/index.php
@@ -1,5 +1,14 @@
 <?php
 
+global $config;
+
 require_once __DIR__ . '/../bootstrap.php';
 
-echo 'hai';
+if(!is_file(\MkkpHoaxRadar\Blacklist::CACHE)) {
+    echo '[]';
+}
+
+$fh = fopen(\MkkpHoaxRadar\Blacklist::CACHE, 'r');
+flock($fh, LOCK_SH);
+fpassthru($fh);
+flock($fh, LOCK_UN);
