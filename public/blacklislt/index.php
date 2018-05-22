307f6515cef1dc4f9285ed1f268c8721d2d58449
lazos <lajos.bencz@gmail.com>
2018-05-22 13:33:42 +0200

    sync

---
+++ public/blacklislt/index.php
@@ -0,0 +1,15 @@
+<?php
+
+global $config;
+
+require_once __DIR__ . '/../../bootstrap.php';
+
+if(!is_file(\MkkpHoaxRadar\Blacklist::CACHE)) {
+    echo '[]';
+}
+
+$fh = fopen(\MkkpHoaxRadar\Blacklist::CACHE, 'r');
+flock($fh, LOCK_SH);
+fpassthru($fh);
+flock($fh, LOCK_UN);
+fclose($fh);
--- public/index.php
+++ public/index.php
@@ -1,15 +1,22 @@
-<?php
-
-global $config;
-
-require_once __DIR__ . '/../bootstrap.php';
-
-if(!is_file(\MkkpHoaxRadar\Blacklist::CACHE)) {
-    echo '[]';
-}
-
-$fh = fopen(\MkkpHoaxRadar\Blacklist::CACHE, 'r');
-flock($fh, LOCK_SH);
-fpassthru($fh);
-flock($fh, LOCK_UN);
-fclose($fh);
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MKKP @ lazos.me</title>
+    <style>
+        html {
+            height: 100%;
+            min-height: 100%;
+        }
+        body {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            min-height: 100%;
+            background: url('/mkkp.jpg') no-repeat center;
+            background-size: contain;
+        }
+    </style>
+</head>
+<body>
+</body>
+</html>
